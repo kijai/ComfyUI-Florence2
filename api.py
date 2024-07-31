49c2048e7fac73bed59d4fc03321b08edce9ac90
Carlo <284474102@qq.com>
2024-07-31 15:08:01 +0800

    add api

--- api.py
+++ api.py
@@ -3,6 +3,7 @@ import nodes
 from aiohttp import web
 from server import PromptServer
 
+
 def get_comfy_dir(subpath=None):
     import inspect
     dir = os.path.dirname(inspect.getfile(PromptServer))
@@ -40,13 +41,25 @@ async def get_tags(request):
         os.path.join(request.query.get("subfolder", ""), request.query["filename"])
         + f" [{type}]")
     model = request.query.get("model", "microsoft/Florence-2-base")
+    task = request.query.get("task", "caption")
+    text_input = request.query.get("text_input", "")
+    output_mask_select = request.query.get("output_mask_select", "")
+    fill_mask = request.query.get("fill_mask", True)
+    do_sample = request.query.get("do_sample", True)
+    keep_model_loaded = request.query.get("keep_model_loaded", True)
+    num_beams = request.query.get("num_beams", 1)
+    max_new_tokens = request.query.get("max_new_tokens", 1024)
     florence2_model, = nodes.DownloadAndLoadFlorence2Model().loadmodel(model=model, precision="fp16", attention="sdpa")
     _, _, res, _ = nodes.Florence2Run().encode(
         image=image,
-        text_input="",
+        text_input=text_input,
         florence2_model=florence2_model,
-        task="caption",
-        fill_mask=True,
-        keep_model_loaded=True
+        task=task,
+        output_mask_select=output_mask_select,
+        do_sample=do_sample,
+        fill_mask=fill_mask,
+        keep_model_loaded=keep_model_loaded,
+        max_new_tokens=max_new_tokens,
+        num_beams=num_beams
     )
     return web.json_response(res)
