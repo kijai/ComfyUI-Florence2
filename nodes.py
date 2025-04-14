1f2f60bee2b0dbb3f750972f453a791129a35d21
Silver <65376327+silveroxides@users.noreply.github.com>
2025-04-14 19:39:29 +0200

    Update name of repo

--- nodes.py
+++ nodes.py
@@ -82,7 +82,7 @@ class DownloadAndLoadFlorence2Model:
                     'MiaoshouAI/Florence-2-large-PromptGen-v1.5',
                     'MiaoshouAI/Florence-2-base-PromptGen-v2.0',
                     'MiaoshouAI/Florence-2-large-PromptGen-v2.0',
-                    'silveroxides/furrence2-large'
+                    'silveroxides/Florence-2-large-TagToPhraseGrounding'
                     ],
                     {
                     "default": 'microsoft/Florence-2-base'
