9878698f452d38887036042aa6b190c950df09b3
Sahler.Alexander <Alexander.Sahler@m-net.de>
2025-08-28 16:33:11 +0200

    fixed error: Single argument to subplot must be a three-digit integer, not GridSpec(1, 1)[0:1, 0:1]

--- nodes.py
+++ nodes.py
@@ -411,8 +411,8 @@ class Florence2Run:
             parsed_answer = processor.post_process_generation(results, task=task_prompt, image_size=(W, H))
 
             if task == 'region_caption' or task == 'dense_region_caption' or task == 'caption_to_phrase_grounding' or task == 'region_proposal':           
-                fig, ax = plt.subplots(1, 1, figsize=(W / 100, H / 100), dpi=100)
-                fig.subplots_adjust(left=0, right=1, top=1, bottom=0)
+                fig = plt.figure(figsize=(W / 100, H / 100), dpi=100)
+                ax = fig.add_axes([0, 0, 1, 1])
                 ax.imshow(image_pil)
                 bboxes = parsed_answer[task_prompt]['bboxes']
                 labels = parsed_answer[task_prompt]['labels']
