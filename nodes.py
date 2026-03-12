2752591c30c2dcddca548facd91b7a25d0d6d3e6
kijai <40791699+kijai@users.noreply.github.com>
2026-03-12 18:23:05 +0200

    Simpler check

--- nodes.py
+++ nodes.py
@@ -206,11 +206,11 @@ class DownloadAndLoadFlorence2Model:
         if version.parse(transformers.__version__) >= version.parse('5.0.0'):
             model, processor = load_model(model_path, attention, dtype, offload_device)
         else:
-            import inspect
             from .modeling_florence2 import Florence2ForConditionalGeneration
-            _sig = inspect.signature(Florence2ForConditionalGeneration.from_pretrained)
-            _dtype_kwarg = 'torch_dtype' if 'torch_dtype' in _sig.parameters else 'dtype'
-            model = Florence2ForConditionalGeneration.from_pretrained(model_path, attn_implementation=attention, **{_dtype_kwarg: dtype}).to(offload_device)
+            try:
+                model = Florence2ForConditionalGeneration.from_pretrained(model_path, attn_implementation=attention, torch_dtype=dtype).to(offload_device)
+            except TypeError:
+                model = Florence2ForConditionalGeneration.from_pretrained(model_path, attn_implementation=attention, dtype=dtype).to(offload_device)
             processor = AutoProcessor.from_pretrained(model_path, trust_remote_code=True)
 
         if lora is not None:
@@ -309,11 +309,11 @@ class Florence2ModelLoader:
         if version.parse(transformers.__version__) >= version.parse('5.0.0'):
             model, processor = load_model(model_path, attention, dtype, offload_device)
         else:
-            import inspect
             from .modeling_florence2 import Florence2ForConditionalGeneration
-            _sig = inspect.signature(Florence2ForConditionalGeneration.from_pretrained)
-            _dtype_kwarg = 'torch_dtype' if 'torch_dtype' in _sig.parameters else 'dtype'
-            model = Florence2ForConditionalGeneration.from_pretrained(model_path, attn_implementation=attention, **{_dtype_kwarg: dtype}).to(offload_device)
+            try:
+                model = Florence2ForConditionalGeneration.from_pretrained(model_path, attn_implementation=attention, torch_dtype=dtype).to(offload_device)
+            except TypeError:
+                model = Florence2ForConditionalGeneration.from_pretrained(model_path, attn_implementation=attention, dtype=dtype).to(offload_device)
             processor = AutoProcessor.from_pretrained(model_path, trust_remote_code=True)
 
         if lora is not None:
